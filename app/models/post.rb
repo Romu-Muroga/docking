f304a9cc587d73588c3975af5a64474c27dcbd21
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-02-13 12:47:14 +0900

    post投稿/編集画面(_form.html.erb)作成、postモデル(ranking_pointのenum定義)、カテゴリーのseedデータセット

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,13 @@ class Post < ApplicationRecord
   validates :ranking_point, presence: true
   validates :eatery_name, presence: true, length: { in: 1..200 }
   validates :eatery_food, presence: true, length: { in: 1..200 }
-  validates: :eatery_address, length: { maximum: 500 }
+  validates :eatery_address, length: { maximum: 500 }
   # validates: latitude
   # validates: longitude
-  validates: :eatery_website, length: { maximum: 500 }
-  validates: :remarks, presence: true
+  validates :eatery_website, length: { maximum: 500 }
+  validates :remarks, presence: true
+
+  # enumを使えば、数字を意味のある文字として扱える。DBには割り当てられた整数が保存される。
+  enum ranking_point: { １位: 3, ２位: 2, ３位: 1 }#ランキングポイント
 
 end
