07ddaad4e331b7acd1406da1c9ab68b9ab7242b9
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-02-21 10:49:30 +0900

    トップページ作成、posts#indexにページ上部に遷移できるボタン作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "sessions#new"
+  root to: "tops#index"
   resources :users
   resources :sessions, only: %i[new create destroy]
   resources :categories do
