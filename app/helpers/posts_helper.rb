9db0923e90a3418c4692178c58de7aeafce36232
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-02-27 10:07:21 +0900

    posts#indexページに表示させる情報を減らす、現在のページをハイライトさせるためのメソッドをヘルパーに記述、デザイン修正

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,9 @@ module PostsHelper
   def post_with_pictures_edit_page?(post)
     !(action_name == "new" || action_name == "create") && (action_name == "edit" || action_name == "update") && post.picture.present?
   end
+
+  # 現在のページをハイライトする方法
+  def cp(path)
+    'current' if current_page?(path)
+  end
 end
