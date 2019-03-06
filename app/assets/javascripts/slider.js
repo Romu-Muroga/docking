8a4d9dfb6705b40c293c32d73c306d480752e4c7
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-03-06 13:21:35 +0900

    top pageのレイアウト変更、formのsubmitボタンを変更、表記を一部英語に変更

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,15 +1,22 @@
 class Animation {
 
-  constructor() {
-  }
+  // constructor() {
+  //   this.slide_clear = false
+  // }
+  //
+  // clearSlideAnimation() {
+  //   this.slide_clear = true
+  // }
 
-  sliderStart() {
+  // sliderStart() {
+  sliderStart(true_or_false) {
     try {
+      // this.slide_clear = false
       const slide = document.getElementById('slide_wrapp');      //スライダー親
       const slideItem = slide.querySelectorAll('.slide_item');   //スライド要素
       const totalNum = slideItem.length - 1;                     //スライドの枚数を取得
       const FadeTime = 2000;                                     //フェードインの時間
-      const IntarvalTime = 7000;                                 //クロスフェードさせるまでの間隔
+      const IntarvalTime = 5000;                                 //クロスフェードさせるまでの間隔
       let actNum = 0;                                            //現在アクティブな番号
       let nowSlide;                                              //現在表示中のスライド
       let NextSlide;                                             //次に表示するスライド
@@ -19,6 +26,11 @@ class Animation {
 
       // 処理を繰り返す
       setInterval(() => {
+          // if (this.slide_clear) return;
+          // console.log("ループしている")
+          if (!(true_or_false)) return;
+          console.log(true_or_false)
+
           if (actNum < totalNum) {
 
               let nowSlide = slideItem[actNum];
@@ -28,7 +40,7 @@ class Animation {
               nowSlide.classList.remove('show_');
               // と同時に、次のスライドがズームしながらフェードインする
               NextSlide.classList.add('show_', 'zoom_');
-              // 次のスライドがズームなし
+              // 次のスライドがズームなしでフェードイン
               // NextSlide.classList.add('show_');
               //フェードアウト完了後、.zoom_削除
               setTimeout(() => {
@@ -57,33 +69,38 @@ class Animation {
   }
 }
 
-
-class Post {
-  constructor(
-    index_callback= () => {},
-  ) {
+class Slider {
+  // constructor(index_callback = () => {}, other_callback = () => {}) {
+  constructor(index_callback = () => {}) {
     this.index_callback = index_callback;
+    // this.other_callback = other_callback;
     this.setPattern();
   }
 
   setPattern() {
-    this.index_pattern = /categories\/\d+\/posts/
-    this.show_pattern = /categories\/\d+\/posts\/\d+/
+    this.index_pattern = /posts$/  //末尾にマッチ
+    this.category_pattern = /categories\/\d+\/posts/
     // etc
   }
 
   run() {
     const url = window.location.pathname
-    if (url.match(this.index_pattern)) {
-      this.index_callback();
+    if (url.match(this.index_pattern) || url.match(this.category_pattern)) {
+      // this.index_callback();
+      this.index_callback(true);
+      console.log("matchしている！！")
     } else {
-      return
+      // this.other_callback();
+      this.index_callback(false);
+      console.log("matchしていない...")
     }
   }
 }
 
+// ターボリンクのロードが終わってから発動する
 $(document).on("turbolinks:load", () => {
   const animation = new Animation()
-  const post = new Post(animation.sliderStart())
+  // const post = new Slider(animation.sliderStart, animation.clearSlideAnimation)
+  const post = new Slider(animation.sliderStart)
   post.run()
 });
