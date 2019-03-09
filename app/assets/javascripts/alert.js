9adfe592b37afa995d993f8a0083a98710349741
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-03-09 14:17:57 +0900

    scssをBEMに修正、jsファイルを作成しviewからscriptタグを削除

---
+++ app/assets/javascripts/alert.js
@@ -0,0 +1,4 @@
+// ターボリンクのロードが終わってから発動する
+$(document).on("turbolinks:load", () => {
+  setTimeout("$('.alert').fadeOut('slow')", 3000)
+});
---
+++ app/assets/javascripts/page_top.js
@@ -0,0 +1,18 @@
+// ターボリンクのロードが終わってから発動する
+$(document).on("turbolinks:load", () => {
+  var pagetop = $('#page_top');
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {  //200pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500); //0.5秒かけてトップへ移動
+      return false;
+  });
+});
