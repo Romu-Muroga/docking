914ecf253db55e53ba460468a67c36f9e380a320
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-06-10 23:59:43 +0900

    Improved the symptom that the slide show of ranking list page is not displayed

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 $(document).on("turbolinks:load", () => {
+=======
+// ターボリンクのロードが終わってから発動する
+$(document).on('turbolinks:load', () => {
+>>>>>>> Improved the symptom that the slide show of ranking list page is not displayed
   setTimeout("$('.alert').fadeOut('slow')", 3000)
 });
