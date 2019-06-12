070a30da720f109b47b0a83e9f12450cc37427c6
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-06-12 09:40:10 +0900

    Add a map with markers

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -2,9 +2,17 @@
 $(document).on('turbolinks:load', () => {
   var map;
   function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4, center: uluru
     });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+  // InvalidValueError: initMap is not a functionの対応
+  window.onload = function () {
+    initMap();
   }
 });//で囲うとgoogle mapが表示されない
