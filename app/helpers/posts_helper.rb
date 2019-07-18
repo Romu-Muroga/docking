528fe0380cc71ba300ebf01ebfe3bcd748b8ba43
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-07-19 07:24:39 +0900

    Hashtag view file correction

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,11 +3,12 @@ module PostsHelper
     !(action_name == 'new' || action_name == 'create') && (action_name == 'edit' || action_name == 'update') && post.picture.present?
   end
 
-  # 現在のページをハイライトする方法
+  # 現在のページをハイライトする
   def cp(path)
     'current' if current_page?(path)
   end
 
+  # ハッシュタグをリンク化する
   def render_with_hashtags(remarks)
     remarks.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/) { |word| link_to word, "/post/hashtag/#{word.delete("#")}" }.html_safe
   end
