524a3d6a72ff55aab24ae34ba0d1be89febfd66d
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-07-18 23:24:55 +0900

    Add helper method for hashtag

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,4 +7,8 @@ module PostsHelper
   def cp(path)
     'current' if current_page?(path)
   end
+
+  def render_with_hashtags(content)
+    content.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/) { |word| link_to word, "/post/hashtag/#{word.delete("#")}" }.html_safe
+  end
 end
