4d650c242d58df47be895b94c1cf4b66cf411149
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-08-05 17:02:29 +0900

    Add user destroy action for admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,12 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def destroy
+    user = User.find(params[:id])
+    return unless user.posts.destroy_all && user.destroy
+
+    flash[:success] = t('flash.destroy', content: user.name)
+    redirect_to admin_users_path
+  end
 end
