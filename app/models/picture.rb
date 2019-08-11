3628b3e2e3497c32ec02eec8470c677427a64887
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-08-12 00:28:56 +0900

    Added block to display photos randomly

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,4 +5,8 @@ class Picture < ApplicationRecord
   validates :image, :imageable_type, :imageable_id, presence: true
   # association
   belongs_to :imageable, polymorphic: true
+  # method
+  def self.random_post_picture_id
+    where(imageable_type: 'Post').pluck(:id).sample
+  end
 end
