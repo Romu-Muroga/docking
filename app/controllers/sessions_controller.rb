1185c96e0dff6872088d557153b481190cb48b67
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-08-26 19:50:25 +0900

    Add strong parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
 
   def create
     # Unlike downcase !, the value is not updated.
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && password_check(user, params[:session][:password])
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && password_check(user, session_params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id), success: t('flash.You_are_now_logged')
     else
@@ -19,6 +19,10 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 
   def password_check(user, password)
     user.authenticate(password)
