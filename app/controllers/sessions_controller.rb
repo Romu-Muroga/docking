fbbdbb22e434cbb2ef2008c1a9eafc95d640a862
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-08-26 20:41:31 +0900

    Code correction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     # Unlike downcase !, the value is not updated.
     user = User.find_by(email: session_params[:email].downcase)
-    if user && password_check(user, session_params[:password])
+    if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id), success: t('flash.You_are_now_logged')
     else
@@ -22,9 +22,5 @@ class SessionsController < ApplicationController
 
   def session_params
     params.require(:session).permit(:email, :password)
-  end
-
-  def password_check(user, password)
-    user.authenticate(password)
   end
 end
