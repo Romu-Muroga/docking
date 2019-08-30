f5a49a290b74c9a919811987e6e7279d0c59d9ed
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-08-30 21:24:44 +0900

    Test user function - mypage display function

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,79 +1,31 @@
 require 'rails_helper'
 # save_and_open_page
+RSpec.describe 'ユーザー管理機能', type: :system do
+  describe 'マイページ表示機能' do
+    let(:user_a) { create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { create(:user, name: 'ユーザーB', email: 'b@example.com') }
 
-RSpec.feature 'ユーザー機能', type: :feature do
-  background do
-    user1 = create(:user)
-    category1 = create(:category)
-    post1 = create(:post, category: category1, user: user1)
-    create(:picture, imageable_id: post1.id, imageable_type: post1.class)
-  end
-
-  scenario 'ログインできるかテスト' do
-    visit root_path
-    within '.start_button' do
-      click_on 'Login'
-    end
-    fill_in 'メールアドレス', with: 'test_user_01@dic.com'
-    fill_in 'パスワード', with: 'password'
-    within '.form_outer' do
-      click_on 'ログイン'
+    before do
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログイン'
     end
 
-    expect(page).to have_content 'ログインしました'
-  end
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
 
-  scenario 'アカウント登録ができるかテスト' do
-    visit root_path
-    within '.start_button' do
-      click_on 'Sign up'
-    end
-    fill_in '名前', with: 'new_user'
-    fill_in 'メールアドレス', with: 'new_user@dic.com'
-    fill_in 'パスワード', with: 'password'
-    fill_in '確認用パスワード', with: 'password'
-    attach_file 'アイコン画像を選択してください', "#{Rails.root}/spec/fixtures/default.png"
-    click_on '登録する'
-
-    expect(page).to have_content 'new_userさんのアカウント登録とログインが完了しました'
-  end
-
-  feature 'ログインした状況', type: :feature do
-    background do
-      visit root_path
-      within '.start_button' do
-        click_on 'Login'
-      end
-      fill_in 'メールアドレス', with: 'test_user_01@dic.com'
-      fill_in 'パスワード', with: 'password'
-      within '.form_outer' do
-        click_on 'ログイン'
+      it 'ユーザーAのマイページが表示される' do
+        expect(page).to have_content 'ユーザーA'
       end
     end
 
-    scenario 'アカウント情報を編集できるかテスト' do
-      click_on 'Edit account info'
-      fill_in '名前', with: '名前を編集しました！'
-      fill_in 'メールアドレス', with: 'test_user_01@dic.com'
-      fill_in 'パスワード', with: 'password'
-      fill_in '確認用パスワード', with: 'password'
-      attach_file 'アイコン画像を選択してください', "#{Rails.root}/spec/fixtures/default.png"
-      click_on '更新する'
-
-      expect(page).to have_content '名前を編集しました！'
-    end
-
-    scenario 'ログアウトできるかテスト' do
-      click_on 'ログアウト'
-      expect(page).to have_content 'ログアウトしました'
-    end
-
-    scenario 'アカウント削除ができるかテスト' do
-      click_on 'Close the account'
-      check 'チェックを入れて退会ボタンを押してください'
-      click_on '退会する'
-
-      expect(page).to have_content 'test_user_01さんのアカウントを削除しました。ご利用いただき、ありがとうございました！'
+    context 'ユーザーBがログインしているとき' do
+      let(:login_user) { user_b }
+      
+      it 'ユーザーAのマイページは表示されない' do
+        expect(page).to have_no_content 'ユーザーA'
+      end
     end
   end
 end
