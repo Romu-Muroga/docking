5f463285c6741d2cd5cb4ffd0108e3260a3bd695
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-09-15 18:15:28 +0900

    Add English locale file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   helper_method :current_user, :logged_in?, :poster?
-  before_action :login_check
+  before_action :login_check, :set_locale
 
   private
+
+  def login_check
+    return if logged_in?
+
+    redirect_to login_path, danger: t('flash.please_login')
+  end
+
+  # TODO: 英語にも対応させる
+  def set_locale
+    I18n.locale = :ja
+  end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -11,12 +22,6 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user.present?
-  end
-
-  def login_check
-    return if logged_in?
-
-    redirect_to login_path, danger: t('flash.please_login')
   end
 
   def poster?(post)
