880670f1969a07f3dd2fbeee239f6a8264eebc41
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-09-27 08:40:39 +0900

    Fix post default value in English locale

--- app/models/post.rb
+++ app/models/post.rb
@@ -86,12 +86,14 @@ class Post < ApplicationRecord
   private
 
   def set_default
-    self.eatery_address = '未登録' if eatery_address.blank?
-    self.eatery_website = '未登録' if eatery_website.blank?
+    self.eatery_address = '未登録' if eatery_address.blank? && I18n.locale == :ja
+    self.eatery_website = '未登録' if eatery_website.blank? && I18n.locale == :ja
+    self.eatery_address = 'unregistered' if eatery_address.blank? && I18n.locale == :en
+    self.eatery_website = 'unregistered' if eatery_website.blank? && I18n.locale == :en
   end
 
   def eatery_website?
-    eatery_website == '' || eatery_website == '未登録'
+    eatery_website == '' || eatery_website == '未登録' || eatery_website == 'unregistered'
   end
 
   def create_hashtag
