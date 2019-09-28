a5f2f29e2c17ddb04b5734c4dcf5b3975ddd1824
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-09-28 16:15:27 +0900

    Change column name of categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_130410) do
+ActiveRecord::Schema.define(version: 2019_09_28_060758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", limit: 500, null: false
+    t.string "name_ja", limit: 500, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
