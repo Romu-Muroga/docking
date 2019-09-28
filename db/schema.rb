10eaf6a16f3529a0f11dbec67f6ef2d5497c86c1
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-09-29 06:47:25 +0900

    Add name_en column to categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_060758) do
+ActiveRecord::Schema.define(version: 2019_09_28_213303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_060758) do
     t.string "name_ja", limit: 500, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name_en", limit: 500
   end
 
   create_table "comments", force: :cascade do |t|
