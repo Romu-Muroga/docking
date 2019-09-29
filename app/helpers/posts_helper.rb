b2fbf518f495b0aabc06ff42366bf2297586a115
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-09-30 01:24:19 +0900

    Fix to translate category notation into English

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,4 +7,24 @@ module PostsHelper
   def choose_post
     t('.to_post') if action_name == 'new' || action_name == 'update'
   end
+
+  def name_ja_or_name_en
+    if I18n.locale == :ja
+      :name_ja
+    elsif I18n.locale == :en
+      :name_en
+    else
+      :name_ja
+    end
+  end
+
+  def choose_name_ja_or_name_en(category)
+    if I18n.locale == :ja
+      category.name_ja
+    elsif I18n.locale == :en
+      category.name_en
+    else
+      category.name_ja
+    end
+  end
 end
