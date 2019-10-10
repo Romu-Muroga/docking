8174385e31adb3a45ee018b054b11591de56e4e7
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-10-10 21:18:39 +0900

    Add null false to categories table name_en column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_213303) do
+ActiveRecord::Schema.define(version: 2019_10_10_120743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_213303) do
     t.string "name_ja", limit: 500, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name_en", limit: 500
+    t.string "name_en", limit: 500, null: false
   end
 
   create_table "comments", force: :cascade do |t|
