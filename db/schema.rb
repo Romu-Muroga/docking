6582264222e2db8e46885f82abed68990d1923b5
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-10-19 22:55:32 +0900

    Add locale column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_120743) do
+ActiveRecord::Schema.define(version: 2019_10_19_134915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_10_10_120743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.string "locale", limit: 100
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
