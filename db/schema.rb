260ebff264b2bf081ff7acaf3d3930870ad1fa29
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-10-22 16:03:12 +0900

    Add null false to locale column of users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_134915) do
+ActiveRecord::Schema.define(version: 2019_10_22_065537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2019_10_19_134915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
-    t.string "locale", limit: 100
+    t.string "locale", limit: 100, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
