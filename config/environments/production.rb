b75c198bbc8a5d41b261c26d7241d110fad4203a
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2020-08-20 01:12:35 +0900

    Fix railsdiff.org diffs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = Uglifier.new(harmony: true)# 初期値（:uglifier）
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
